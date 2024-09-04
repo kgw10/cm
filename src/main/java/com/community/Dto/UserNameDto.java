fc02236bc612409fcdd3942bc303bcbb966e469f
kgw10 <aa01059183132@gmail.com>
2024-09-04 10:00:39 +0900

    09

---
+++ src/main/java/com/community/Dto/UserNameDto.java
@@ -0,0 +1,13 @@
+package com.community.Dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class UserNameDto {
+
+    private String name;
+}
---
+++ src/main/java/com/community/control/NameController.java
@@ -0,0 +1,8 @@
+package com.community.control;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NameController {
+}
